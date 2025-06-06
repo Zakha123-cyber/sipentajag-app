951236805edf059e2a12000974c573fa1b06ca2b
Zakha123-cyber <aguscow6@gmail.com>
2025-06-06 13:49:44 +0700

    initial project with set up db

---
+++ app/Models/Fertilizer.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Fertilizer extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'usage_instruction',
+    ];
+
+    public function diseases()
+    {
+        return $this->belongsToMany(Disease::class, 'disease_fertilizer');
+    }
+}
